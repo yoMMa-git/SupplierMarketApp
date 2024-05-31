a2d7c3cf69f2235f15855a1815b82c7e0d5a28f1
Cornmeal Steele <cornmeal@stduent.21-school.ru>
2024-06-01 01:18:26 +0700

    Added list of active players in waiting lobby

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame2 = tk.Frame(root)
 main_font = Font(family='Stem', weight='bold', size=21)
+list_font = Font(family='Stem', size=12)
 
 
 # Function to handle button click
@@ -25,14 +26,46 @@ def on_button_click(entry_field):
         if data['response'] == 'exists':
             messagebox.showwarning(message="This nickname already exists!")
         elif data['response'] == 'success':
-            messagebox.showinfo(message="OK! Changing UI...")
-            secondWindow()
+            # messagebox.showinfo(message="OK! Changing UI...")
+            frame.pack_forget()
+
+            new_label = ttk.Label(frame2, text='Ожидание игроков...', font=main_font)
+            new_label.pack(pady=10)
+
+            test_array = []
+            test_array_var = tk.StringVar(value=test_array)
+
+            listbox = tk.Listbox(frame2, font=list_font)
+            listbox.pack(pady=10, fill=tk.BOTH, padx=20)
+
+            scrollbar = ttk.Scrollbar(listbox, orient='vertical', command=listbox.yview)
+            scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+            listbox['yscrollcommand'] = scrollbar.set
+
+            frame2.pack()
+
+            @sio.on('lobby_update')
+            def logging(data):
+                listbox.delete(0, listbox.size() - 1)
+                for name in data['nicknames']:
+                    listbox.insert(0, name)
 
 
-def secondWindow():
+def secondWindow():  # got inserted into listener directly
     frame.destroy()
-    new_label = ttk.Label(root, text='Ожидание игроков...', font=main_font)
+    new_label = ttk.Label(frame2, text='Ожидание игроков...', font=main_font)
     new_label.pack(pady=10)
+
+    listbox = tk.Listbox(frame2, font=list_font)
+    listbox.pack(pady=10, fill=tk.BOTH, padx=20)
+
+    scrollbar = ttk.Scrollbar(listbox, orient='vertical', command=listbox.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    listbox['yscrollcommand'] = scrollbar.set
+
+    frame2.pack()
 
 
 def mainWindow():
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask import Flask, request, session
+from flask_socketio import SocketIO, join_room, leave_room, emit, rooms
+import time
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -11,19 +12,30 @@ lobby_clients = set()  # Set to keep track of clients in the lobby (as fas as AI
 ROOM_NAME = 'MainLobby'
 game_start = False
 
+
 @socketio.on('connect')
 def connect_user():
-    client_id = request.sid
-    clients.append(client_id)
-    print("A new user just connected: ", client_id)
-    #emit('connected', room=client_id)
+    client_id = request.sid  # getting SID of user
+    clients.append(client_id)  # adding this SID (no idea what for)
+    print("A new user just connected: ", client_id)  # logging
+
+
+@socketio.on('disconnect')
+def disconnect_user():
+    client_id = request.sid  # getting SID of user
+    try:
+        nicknames.pop(client_id)
+    except KeyError:
+        pass
+    clients.remove(client_id)  # removing it from list of active clients
+    print("A user just disconnected: ", client_id)  # logging
 
 
 @socketio.on('register')
 def register_user(data):
-    print(data)
-    client_id = data['sid']
-    login = data['nickname']
+    print("Registration event: ", request.sid)  # logging
+    client_id = request.sid
+    login = data['nickname']  # getting login from
     join_room(client_id)
     if login in nicknames.values():
         emit('nickname_response', {'response': 'exists'}, room=client_id)
@@ -31,12 +43,13 @@ def register_user(data):
     else:
         emit('nickname_response', {'response': 'success'}, room=client_id)
         nicknames[client_id] = login
-        print(nicknames)
         if len(nicknames.values()) > 2:
             game_start = True
         join_room(room=ROOM_NAME)
         data = [name for name in nicknames.values()]
-        emit('lobby_update', {'nicknames': data}, room=ROOM_NAME)
+        time.sleep(1)
+        emit('lobby_update', {'nicknames': data}, room=client_id)
+        print("Current rooms user in:", rooms(client_id))
 
 
 if __name__ == '__main__':
